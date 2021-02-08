a01f803078957ad3f4a8e697ba3c3cdb708b5104
Tajbiul-prog <71703886+Tajbiul-prog@users.noreply.github.com>
2021-02-08 14:24:10 +0600

    pray sesh :D

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
-
-fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=c`)
-    .then(res => res.json())
-    .then(data => getResultList(data.meals))
-
-    function getResultList(meals){
-        console.log(meals)
-        const mealNames = 
-        console.log(mealNames);
-    }
+const searchMeal = () => {
+    const searchValue = document.getElementById("searchInput").value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMeals(data.meals))
+}
 
 
+const displayMeals = meals => {
+    const mealBox = document.getElementById("menuBoxs");
 
-
-
-
-
-
-
-    getResultList
-
-    // const mealName = data.meals;
-    // for(let i = 0; i < mealName.length; i++){
-    // const element = mealName[i];
-    // console.log(element.strMeals)
+    meals.forEach(meal => {
+        const mealDiv = document.createElement('div');
+        mealDiv.className = 'card';
+        const addDiv = `
+        <img class="card-img-top" alt="..." src="${meal.strMealThumb}">
+        <h5 class="card-title">${meal.strMeal}</h5>
+        <p class="card-text">${meal.strArea}</p>
+        `
+        mealDiv.innerHTML = addDiv
+        mealBox.appendChild(mealDiv);
+    })
+}
